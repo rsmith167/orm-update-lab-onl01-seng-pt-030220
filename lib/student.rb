2d2f17efed8eade88cd5922dab9aa80a7eec6615
Rwanda S <rwanda.ace@gmail.com>
2020-06-21 20:56:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,7 @@ def self.create(name, grade)
   student
 end
 def self.new_from_db(row)
-student = self.new(row[1], row[2],row[3])
+student = self.new(row[1], row[2],row[0])
 student
 end
 def self.find_by_name(name)
